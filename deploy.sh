21b9c7ff6afc8f2ee66bf9e1035453fbe29ceaa6
Jonathan Herbert <foohyfooh@gmail.com>
2017-02-26 15:37:09 -0400

    Update Deploy Script

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,6 @@ TARGET_BRANCH="master"
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
     echo "Skipping deploy; just doing a build."
-    npm install -g polymer-cli
     polymer build
     exit 0
 fi
@@ -28,7 +27,6 @@ cd ..
 rm -rf build/bundled/**/* || exit 0
 
 # Run our compile script
-npm install -g polymer-cli
 polymer build
 
 # Now let's go have some fun with the cloned repo
