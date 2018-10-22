275d676d17fa14d4ba7950c99f5b8b2ae2513e6c
Qarun Qadir Bissoondial <qarun80@hotmail.com>
2018-10-22 16:25:00 -0400

    - Added some basic components (empty body) - configured routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,35 @@ import { AppComponent } from './app.component';
 import { TopbarComponent } from './topbar/topbar.component';
 import {MDBBootstrapModule} from 'angular-bootstrap-md';
 import { HomeComponent } from './home/home.component';
+import { RouterModule, Routes } from '@angular/router';
+import { ExecsComponent } from './execs/execs.component';
+import { AboutComponent } from './about/about.component';
+import { CalendarComponent } from './calendar/calendar.component';
+
+const appRoutes: Routes = [
+  { path: 'home',  component: HomeComponent },
+  { path: 'execs', component: ExecsComponent},
+  { path: 'calendar', component: CalendarComponent },
+  { path: 'about', component: AboutComponent},
+  { path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     TopbarComponent,
     HomeComponent,
+    ExecsComponent,
+    AboutComponent,
+    CalendarComponent,
   ],
   imports: [
     BrowserModule,
-    MDBBootstrapModule.forRoot()
+    MDBBootstrapModule.forRoot(),
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
