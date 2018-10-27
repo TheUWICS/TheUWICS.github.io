b36f850c3d3961a6fd93671e9ed55d3d218fd3cb
Qarun Qadir Bissoondial <qarun80@hotmail.com>
2018-10-27 03:25:50 -0400

    - Added fixes for mobile view of executives page (Not 100% completed yet)

---
+++ src/app/exec-card/exec-card.component.ts
@@ -0,0 +1,19 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {UwicsExec} from '../../assets/interfaces';
+
+@Component({
+  selector: 'app-exec-card',
+  templateUrl: './exec-card.component.html',
+  styleUrls: ['./exec-card.component.scss']
+})
+export class ExecCardComponent implements OnInit {
+  @Input() exec: UwicsExec;
+  constructor() {
+
+  }
+
+  ngOnInit() {
+
+  }
+
+}
