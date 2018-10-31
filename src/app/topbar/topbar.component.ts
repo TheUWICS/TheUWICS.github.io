ce9409b0b0b143438d31bd27ceee6f64ed018964
Qarun Qadir Bissoondial <qarun80@hotmail.com>
2018-10-31 01:23:47 -0400

    - Added implementation of light and dark theme

--- src/app/topbar/topbar.component.ts
+++ src/app/topbar/topbar.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./topbar.component.css']
 })
 export class TopbarComponent implements OnInit {
-
-  constructor() { }
+  private themeWrapper;
+  dark = true;
+  constructor() {}
 
   ngOnInit() {
+    this.themeWrapper = document.querySelector('html');
   }
 
+  toggleTheme() {
+    if (this.dark) {
+      this.themeWrapper.classList.remove('dark-theme');
+      this.themeWrapper.classList.add('light-theme');
+    } else {
+      this.themeWrapper.classList.remove('light-theme');
+      this.themeWrapper.classList.add('dark-theme');
+    }
+
+    this.dark = !this.dark;
+  }
 }
