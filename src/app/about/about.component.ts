09efc43780953b0caeb4e3b82c46b05acdd62e2a
Qarun Qadir Bissoondial <qarun80@hotmail.com>
2018-10-31 16:48:34 -0400

    - Added some text descriptions - Added a fixed scrollbar - Added an expandable text area in the about page

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
-
+  expanded = false;
+  read = 'read more';
+  about = 'The University of the West Indies Computing Society (UWICS) was established in 1996. The purpose of this society is to forge' +
+    ' a plan that focuses on computing and information technology in everyday life. UWICS comprises of individuals who are enthusiastic ' +
+    'about the interesting fascinations that can be done with technology whether it is learning about the world of programming, gaining a' +
+    ' better understanding of how augmented reality works, exploring the graphical world involved in creating logos or being educated' +
+    ' about the various jobs that are elaborated. We aim to foster an environment that is educational, entertaining and inspiring so ' +
+    'that members are encouraged to discover and develop their knowledge in computing and information technology.';
   constructor() { }
 
   ngOnInit() {
+  }
+
+  expand() {
+    this.expanded = !this.expanded;
+    if (this.read === 'read more') {
+      this.read = 'read less';
+    } else {
+      this.read = 'read more';
+    }
+
   }
 
 }
